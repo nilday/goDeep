ceec84d158ec5a2d2bffaa807c2c637c8bd0ae58
nilday <howard.h.l@126.com>
2015-03-19 22:03:38 +0800

    start to dev nn

--- nn/nn.go
+++ nn/nn.go
@@ -7,19 +7,33 @@ import (
 	"goDeep/utils"
 )
 
-type NeuralNetwork struct {
-	HiddenLayer      []float64
-	InputLayer       []float64
-	OutputLayer      []float64
-	WeightHidden     [][]float64
-	WeightOutput     [][]float64
-	ErrOutput        []float64
-	ErrHidden        []float64
-	LastChangeHidden [][]float64
-	LastChangeOutput [][]float64
-	Regression       bool
-	Rate1            float64 //learning rate
-	Rate2            float64
+//type NeuralNetwork struct {
+//	HiddenLayer      []float64
+//	InputLayer       []float64
+//	OutputLayer      []float64
+//	WeightHidden     [][]float64
+//	WeightOutput     [][]float64
+//	ErrOutput        []float64
+//	ErrHidden        []float64
+//	LastChangeHidden [][]float64
+//	LastChangeOutput [][]float64
+//	Regression       bool
+//	Rate1            float64 //learning rate
+//	Rate2            float64
+//}
+
+type Layer []float64
+type Weight [][]float64
+
+type NN struct {
+	layers  []Layer
+	weights []Weight
+}
+
+func NN(size []int, input []float64, learningRate float64)
+
+func (self *NN) forward() {
+
 }
 
 func main() {
